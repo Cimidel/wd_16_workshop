d9dc1403da8d5a2a48882a2b1fe86cedf630fdf2
Cimidel <ionutdimache@ymail.com>
2024-03-27 20:51:02 +0200

    Content products

--- product.js
+++ product.js
@@ -2,26 +2,26 @@ const BASE_URL = 'https://fakestoreapi.com';
 
 document.addEventListener('DOMContentLoaded', 
     async () => {
-    const productId = localStorage.getItem('productid');
-    const responce = await fetch('${BASE_URL})/products/${productId}');
+    const productId = localStorage.getItem('productId');
+    const responce = await fetch(`${BASE_URL})/products/${productId}`);
     const productData = await responce.json();
     console.log(productData);
-    const imageContainer = document.getElementById{'img-container'};
-    imagecontainer.innerHTML ='<img class='img-fluid' src =${prooductData.image}>';
+    const imageContainer = document.getElementById('img-container');
+    imageContainer.innerHTML =`<img class='img-fluid' src =${productData.image}>`;
     const title = document.getElementById('product-title');
     title.textContent = productData.title;
 
-    const category = document.getElementById{'prodiuct-category'};
+    const category = document.getElementById('product-category');
     category.textContent = productData.category;
 
-    const price =document.getElementById{'product-price'};
+    const price =document.getElementById('product-price');
     price.textContent = productData.price;
 
-    const description = document.getElementById{'product-description'};
-    description.textContent = productData.descriptio;
+    const description = document.getElementById('product-description');
+    description.textContent = productData.description;
 
-    const reviewCount = document.getElementById{'review-count'};
-    reviewCount.textContent ='of ${productData.rating.count} reviews';
+    const reviewCount = document.getElementById('review-count');
+    reviewCount.textContent =`of ${productData.rating.count} reviews`;
 
 
 });
